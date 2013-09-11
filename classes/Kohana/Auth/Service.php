2991db7793399d6e807a33e6ed25d7dfb1997cce
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-11 17:22:24 +0300

    Return facebook logged out query param

--- classes/Kohana/Auth/Service.php
+++ classes/Kohana/Auth/Service.php
@@ -133,7 +133,7 @@ abstract class Kohana_Auth_Service {
 		if ( ! $this->enabled())
 			return FALSE;
 
-		return $this->logout_service(URL::site(Request::current()->uri(), TRUE));
+		return $this->logout_service(Request::initial(), URL::site(Request::current()->uri(), TRUE));
 	}
 
 	public function login()
@@ -160,7 +160,7 @@ abstract class Kohana_Auth_Service {
 
 	abstract public function login_url($back_url);
 	
-	abstract public function logout_service($back_url);
+	abstract public function logout_service($request, $back_url);
 
 	abstract public function service_user_info();
 
